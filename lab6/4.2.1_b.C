26c3325b45322a5d89b0eca743e77e323f1fee2c
Mateusz Powirski <92019610+P0wir@users.noreply.github.com>
2022-03-30 11:35:45 +0200

    Create 4.2.1_b.C

---
+++ lab6/4.2.1_b.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(int n, int tab[])
+{
+    for (int i=0; i<n; i++)
+    {
+        tab[i]=i;
+    }
+}
+
+int main()
+{
+    int tablica[4];
+    foo(4,tablica);
+    for(int i=0; i<4; i++)
+    {printf("[%d]=%d\n", i, tablica[i]);
+    }
+}
