02612330f45f4edc719affd345523e5e4f500a70
Mateusz Powirski <92019610+P0wir@users.noreply.github.com>
2022-05-25 14:16:52 +0200

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -3,21 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-x_lista = []
-for x in range(1,21):
-    x_lista.append((x*x)+4*x)
-print(x_lista)
+A = np.linspace(-2,2, num=100)
 
-d = np.sin(x_lista)
-print(d)
+a = np.tan(A)
+b = np.sin(A)
 
-
-
-x=x_lista
-y=x/d
-sns.lineplot(x, y, label='linia nr 1', color='red', marker='o', linestyle=':')
+d = a+b
+print(a+b)
+plt.plot(A, d, label="tan(x)+sin(x)")
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.legend(loc='upper left')
-plt.title("wykres funkcji f(x) dla x [1,20]")
 plt.show()
+
+
